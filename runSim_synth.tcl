2b6a3001bf36fbb9de69fa011ec4e5520683445a
vyzu4 <99065703+vyzu4@users.noreply.github.com>
2025-11-25 21:06:58 -0800

    Update runSim_synth.tcl

--- runSim_synth.tcl
+++ runSim_synth.tcl
@@ -1,35 +1,12 @@
-set TOP_LEVEL "mkMultBuff"
-set TB_NAME ${TOP_LEVEL}_SYNsim_TB
+set TOP_LEVEL "dnn_accelerator"
+set TB_NAME ${TOP_LEVEL}_tb
 
-set SDF_FOLDER "/ubc/ece/home/ss/grads/avilash/Projects/elec402-intro-to-vlsi/multiplier-module/syn/outputs"
+set SDF_FOLDER "/ubc/ece/home/ugrads/v/vzhu03/ELEC402/synthesis/outputs"
 
 # Starting the simulator
-vsim -default_radix unsigned -voptargs=+acc -sdfnoerror -sdfmax /$TB_NAME/$TOP_LEVEL=$SDF_FOLDER/${TOP_LEVEL}_v0_map.sdf -l $TB_NAME.sim.log work.${TB_NAME} 
+vsim -default_radix unsigned -voptargs=+acc -sdfnoerror -sdfmax /$TB_NAME/$TOP_LEVEL=$SDF_FOLDER/${TOP_LEVEL}_v2_500_map.sdf -l $TB_NAME.sim.log work.${TB_NAME} 
 
 # Open the vcd file to write the waveforms to
 vcd file ${TOP_LEVEL}.vcd
 # Add the signals to be logged for activity factor
 vcd add /$TB_NAME/${TOP_LEVEL}/*
-
-# Add the waves to the Wave viewer
-add wave -label CLK -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/CLK
-
-add wave -divider inferface_mult
-add wave -label EN_mult -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/EN_mult
-add wave -label RDY_mult -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/RDY_mult
-
-add wave -divider interface_writeMem
-add wave -label EN_writeMem -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/EN_writeMem
-add wave -label writeMem_addr -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/writeMem_addr
-
-add wave -divider interface_blockRead
-add wave -label EN_blockRead -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/EN_blockRead
-
-add wave -divider interface_readMem
-add wave -label EN_readMem -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/EN_readMem
-add wave -label readmem_addr -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/readMem_addr
-
-add wave -divider interface_memVal
-add wave -label VALID_memVal -position insertpoint sim:/$TB_NAME/$TOP_LEVEL/VALID_memVal
-
-#run -all
