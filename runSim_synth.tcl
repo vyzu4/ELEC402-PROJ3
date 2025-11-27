1d0aab1c3dd57b67d89926a1ad3bcba08f64ad0e
vyzu4 <99065703+vyzu4@users.noreply.github.com>
2025-11-26 22:19:51 -0800

    Update runSim_synth.tcl

--- runSim_synth.tcl
+++ runSim_synth.tcl
@@ -4,7 +4,7 @@ set TB_NAME ${TOP_LEVEL}_tb
 set SDF_FOLDER "/ubc/ece/home/ugrads/v/vzhu03/ELEC402/synthesis/outputs"
 
 # Starting the simulator
-vsim -default_radix unsigned -voptargs=+acc -sdfnoerror -sdfmax /$TB_NAME/$TOP_LEVEL=$SDF_FOLDER/${TOP_LEVEL}_v2_500_map.sdf -l $TB_NAME.sim.log work.${TB_NAME} 
+vsim -default_radix unsigned -voptargs=+acc -sdfnoerror -sdfmax /$TB_NAME/$TOP_LEVEL=$SDF_FOLDER/${TOP_LEVEL}_dut.sdf -l $TB_NAME.sim.log work.${TB_NAME} 
 
 # Open the vcd file to write the waveforms to
 vcd file ${TOP_LEVEL}.vcd
